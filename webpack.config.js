861a89ac76f9a5741cf45b49737dfbbe5c6b2ebe
irvelds <irvelds@mail.ru>
2023-04-25 14:41:28 +0300

    feat: add basic project structure and webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+module.exports = {
+  mode,
+  entry: './src/script.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    clean: true,
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  devtool: mode !== 'production' ? 'source-map' : false,
+  devServer: {
+    static: './src',
+  },
+};
